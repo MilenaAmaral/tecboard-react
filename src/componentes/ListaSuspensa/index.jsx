db006205f485e591e35cbff0f6a7fd01d6b577d2
Milena Amaral <amaralmillena76@gmail.com>
2026-09-19 18:47:11 -0300

    Ajusta formulário de evento: corrige label, renderiza lista de temas dinamicamente, define valor padrão do select e adiciona campo de capa.

--- src/componentes/ListaSuspensa/index.jsx
+++ src/componentes/ListaSuspensa/index.jsx
@@ -1,10 +1,19 @@
-import './lista-suspesa.estilos.css'
+import "./lista-suspensa.estilos.css"
 
-export function ListaSuspensa () {
+export function ListaSuspensa ({ itens, ...rest }) {
 
     return (
-        <select className='lista-suspensa-form'>
-            <option value=""></option>
+        <select { ...rest } className='lista-suspensa-form' defaultValue="" required>
+            <option value="" disabled>
+                Selecione uma opção
+            </option>
+            {itens.map(function (item) {
+                return (
+                    <option key={item.id } value={item.id}>
+                        {item.nome}
+                    </option>
+                )
+            })}
         </select>
     )
 }
